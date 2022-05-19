16eeec929a996b70ad2949886360f6d8ebe1b5c2
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-05-19 11:19:08 +0300

    tests: load page to tmp directory

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,8 +1,11 @@
+import os
+import tempfile
+
 from page_loader.scripts.page_loader import download
 
 
 def test_download():
-    dir_path = '/var/tmp'
     web_page = 'https://ru.hexlet.io/courses'
-    total_path = download(web_page, dir_path)
-    assert '/var/tmp/ru-hexlet-io-courses.html' == total_path
+    with tempfile.TemporaryDirectory() as tmpdir:
+        total_path = download(web_page, tmpdir)
+    assert os.path.join(tmpdir, 'ru-hexlet-io-courses.html') == total_path
